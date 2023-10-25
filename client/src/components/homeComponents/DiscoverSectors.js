4b3b211a15a9a0efb33ee4a0774e440901b63a6a
Adam-Abera <iruleadamabera@gmail.com>
2023-10-25 13:40:38 +0300

    Added GIFs instead of iframes

--- client/src/components/homeComponents/DiscoverSectors.js
+++ client/src/components/homeComponents/DiscoverSectors.js
@@ -3,36 +3,36 @@
 import React from 'react';
 import "../../App.css";
 import { Link } from 'react-router-dom';
-import video1 from "../../images/home/potentail5.mp4";
-import video2 from "../../images/home/potentail4.mp4";
-import video3 from "../../images/home/potential6.mp4";
+import video1 from "../../images/home/potential7.gif";
+import video2 from "../../images/home/potential8.gif";
+import video3 from "../../images/home/potential9.gif";
 
 const DiscoverSectors = () => {
   const videoContainerStyle = {
     position: 'relative',
     borderRadius: '40px',
-    overflow: 'hidden',
-    marginBottom: '-30px',
+    marginBottom: '-100px',
   };
 
   const videoStyle = {
     innerWidth: "cover",
-    innerHeight: "cover"
+    innerHeight: "cover",
+    borderRadius: "10px"
     // borderRadius: '10px',
   };
 
   return (
     <div className="container shadow p-4 mb-5 mt-5" style={{ background: '#2b2b2b', color: "white" }}>
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-6" style={{height:"700px"}}>
           <div style={videoContainerStyle}>
-            <iframe style={videoStyle}  src={video1 + "?autoplay=1&loop=1"} title="Video 1" frameBorder="0" allowFullScreen></iframe>
+          <img src={video1} alt="Potential Sector Image 1" style={videoStyle} height="300px" width="220px"/>
           </div>
           <div style={{ ...videoContainerStyle, marginLeft: '70px' }}>
-            <iframe style={videoStyle}  src={video2 + "?autoplay=1&loop=1"} title="Video 2" frameBorder="0" allowFullScreen></iframe>
+          <img src={video2} alt="Potential Sector Image 2" style={videoStyle} height="300px" width="220px"/>
           </div>
           <div style={{ ...videoContainerStyle, marginLeft: '150px' }}>
-            <iframe style={videoStyle}  src={video3 + "?autoplay=1&loop=1"} title="Video 3" frameBorder="0" allowFullScreen></iframe>
+          <img src={video3} alt="Potential Sector Image 3" style={{...videoStyle, marginBottom:"50px"}} height="300px" width="220px"/>
           </div>
         </div>
         <div className="col-md-6">
