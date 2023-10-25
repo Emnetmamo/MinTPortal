320abfa6a7204dfb5146731cdf1c31a9ee8a2175
one-two345 <129900924+one-two345@users.noreply.github.com>
2023-10-25 22:53:27 +0300

    why?

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@
 import React from 'react';
 import './App.css';
 import RoutesComponent from './Routes';
+import AdminRoutesComponent from './pages/admin/adminRoutes'
 
 function App() {
   return (
     <div className="App">
+      <AdminRoutesComponent/>
       <RoutesComponent />
+      
     </div>
   );
 }
