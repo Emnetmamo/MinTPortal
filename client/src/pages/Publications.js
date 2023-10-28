e2e1f2e998e43bd67c43e9a61234a569d581152e
adan eshete <adaneeshete560@gmail.com>
2023-10-28 09:59:57 -0700

    publication final

--- client/src/pages/Publications.js
+++ client/src/pages/Publications.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import news from "../images/News/news9.webp";
 import new8 from "../images/News/news8.jpeg";
 import new7 from "../images/News/news7.jpeg";
-
+import "./assets/publication.css";
 import ProjectDescription from "./ProjectDescription";
 
 const Publications = () => {
@@ -19,51 +19,15 @@ const Publications = () => {
         </h1>
       </div>
       <div className=" d-flex row justify-content-center mt-5 ms-5 me-5 align-items- center ">
+       
+
         <div className="col p-5">
-          <div className="row card">
-            <img src={news} alt="AI chat system" />
+          <div className="row card" id="card">
+            <img src={news} alt="AI chat system" id="image" />
             <h2>Ai chat system</h2>
             <h6 class="mb-0">Author: Dr. Mekonnen Yilma</h6>
-            <h6>Date h6ublished: December 4, 2009</h6>
-            <button 
-            className="btn btn-secondary btn-sm"
-              type="button"
-              style={{
-                  padding:'12px',
-                rderRadius: "8px",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "30%",
-                marginLeft:'2pt',
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content1 = document.getElementById("content1");
-                if (
-                  content1.style.display === "none" ||
-                  content1.style.display === ""
-                ) {
-                  content1.style.display = "block";
-                } else {
-                  content1.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content1"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content1"
-            >
+            <h6>Date published: December 4, 2009</h6>
+            <div className="" id="notes">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -73,200 +37,20 @@ const Publications = () => {
             <div className="mx-auto d-block">
               {" "}
               <b>do you want to download the file?</b>
-              <button
-                href=""
-                download="downloaded-file-name.pdf"
-                className="btn btn-success"
-                style={{
-                
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
-              >
-                Download{" "}
+              <button className="btn " id="button ">
+                download
               </button>
             </div>{" "}
           </div>{" "}
-        </div>
-        <div className="col p-5">
-          <div className="row card">
-            <img src={new8} alt="New 8" style={{}} />
-            <h2 className="row">Solar Energy</h2>
-            <p class="mb-0">Author: Tomas Mores</p>
-            <p>Date published: December 4, 2003</p>
-            <button
-              type="button"
-              style={{
-                backgroundColor: "blue",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "50%",
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content2 = document.getElementById("content2");
-                if (
-                  content2.style.display === "none" ||
-                  content2.style.display === ""
-                ) {
-                  content2.style.display = "block";
-                } else {
-                  content2.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content2"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content2"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <br />
-            <div className="mx-auto d-block">
-              {" "}
-              <b>do you want to download the file?</b>
-              <button
-                href=""
-                download="downloaded-file-name.pdf"
-                className="btn btn-success"
-                style={{
-                  backgroundColor: "green",
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
-              >
-                Download
-              </button>
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>
-      <div className="row justify-content-center mt-5 ">
-        <div className="col p-5">
-          <div className="row card">
-            <img src={news} alt="Exploring the Impact of Climate Change" />
-            <h2>Exploring the Impact of Climate Change</h2>
-            <p class="mb-0">Author: John Smith</p>
-            <p>Date published: October 15, 2023</p>
-
-            <button
-              type="button"
-              style={{
-                backgroundColor: "blue",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "50%",
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content1 = document.getElementById("content1");
-                if (
-                  content1.style.display === "none" ||
-                  content1.style.display === ""
-                ) {
-                  content1.style.display = "block";
-                } else {
-                  content1.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content1"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content1"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <br />
-            <div className="mx-auto d-block">
-              {" "}
-              <b>do you want to download the file?</b>
-              <button
-                href=""
-                download="downloaded-file-name.pdf"
-                className="btn btn-success"
-                style={{
-                  backgroundColor: "green",
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
-              >
-                Download
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="col p-5">
-          <div className="row card">
-            <img src={new8} alt="New 8" style={{}} />
+          <br />
+        <br />
+          <div className="row card "  id="card">
+            <img src={new8} alt="New 8" id="image" />
             <h2 className="row">The Future of Artificial Intelligence</h2>
             <p class="mb-0">Author: Emily Johnson</p>
             <p>Date published: September 28, 2023</p>
 
-            <button
-              type="button"
-              style={{
-                backgroundColor: "blue",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "50%",
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content2 = document.getElementById("content2");
-                if (
-                  content2.style.display === "none" ||
-                  content2.style.display === ""
-                ) {
-                  content2.style.display = "block";
-                } else {
-                  content2.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content2"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content2"
-            >
+            <div class="content2" id="notes">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -281,62 +65,82 @@ const Publications = () => {
                 href=""
                 download="downloaded-file-name.pdf"
                 className="btn btn-success"
-                style={{
-                  backgroundColor: "green",
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
+                id="button"
               >
                 Download
               </button>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="row justify-content-center mt-5 ">
+          <br />
+        <br />
+          <div className="row card p-5" id="card">
+    <img src={new8} alt="New 8" id="image" />
+    <h2 className="row">The Role of Genetics in Disease Prevention</h2>
+    <p className="mb-0">Author: Sophia Williams</p>
+    <p>Date published: December 12, 2023</p>
+
+    <div id="notes">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.
+    </div>
+    <br />
+
+    <div className="mx-auto d-block">
+      {" "}
+      <b>Do you want to download the file?</b>
+      <button
+        href=""
+        download="downloaded-file-name.pdf"
+        className="btn"
+        id="button"
+      >
+        Download
+      </button>
+    </div>
+  </div>
+        </div>{" "}
+       
+       
+
+
         <div className="col p-5">
-          <div className="row card">
-            <img src={news} alt="AI chat system" />
+          <div className="row card " id="card">
+            <img src={new8} alt="New 8" id="image" />
+            <h2 className="row">Solar Energy</h2>
+            <p class="mb-0">Author: Tomas Mores</p>
+            <p>Date published: December 4, 2003</p>
+            <div id="notes">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </div>
+            <br />
+            <div className="mx-auto d-block">
+              {" "}
+              <b>do you want to download the file?</b>
+              <button
+                href=""
+                download="downloaded-file-name.pdf"
+                className="btn "
+                id="button"
+              >
+                Download
+              </button>
+            </div>{" "}
+         
+          </div>{" "}
+          <br />
+        <br />
+          <div className="row card me-2" id="card">
+            <img src={news} alt="AI chat system" id="image"/>
             <h2>Understanding Renewable Energy Sources</h2>
             <p class="mb-0">Author: Michael Anderson</p>
             <p>Date published: November 5, 2023</p>
 
-            <button
-              type="button"
-              style={{
-                backgroundColor: "blue",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "50%",
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content1 = document.getElementById("content1");
-                if (
-                  content1.style.display === "none" ||
-                  content1.style.display === ""
-                ) {
-                  content1.style.display = "block";
-                } else {
-                  content1.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content1"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content1"
-            >
+            <div class="content1" id="notes">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -349,88 +153,55 @@ const Publications = () => {
               <button
                 href=""
                 download="downloaded-file-name.pdf"
-                className="btn btn-success"
-                style={{
-                  backgroundColor: "green",
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
+                className="btn "
+                id="button"
               >
                 Download
               </button>
             </div>
           </div>
-        </div>
+          <br />
+        <br />
+          <div className="row card p-5" id="card">
+    <img
+      src={news}
+      alt="Exploring the Impact of Climate Change"
+      id="image"
+    />
+    <h2>Exploring the Impact of Climate Change</h2>
+    <p className="mb-0">Author: John Smith</p>
+    <p>Date published: October 15, 2023</p>
 
-        <div className="col p-5">
-          <div className="row card">
-            <img src={new8} alt="New 8" style={{}} />
-            <h2 className="row">The Role of Genetics in Disease Prevention</h2>
-            <p class="mb-0">Author: Sophia Williams</p>
-            <p>Date published: December 12, 2023</p>
+    <div className="content1" id="notes">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.
+    </div>
+    <br />
+    <div className="mx-auto d-block">
+      {" "}
+      <b>Do you want to download the file?</b>
+      <button
+        href=""
+        download="downloaded-file-name.pdf"
+        className="btn"
+        id="button"
+      >
+        Download
+      </button>
+    </div>
+  </div>
+        </div>{" "}
 
-            <button
-              type="button"
-              style={{
-                backgroundColor: "blue",
-                borderRadius: "8px",
-                cursor: "pointer",
-                padding: "18px",
-                width: "50%",
-                border: "1px solid black", // Specify the border style here
-                textAlign: "left",
-                outline: "none",
-              }}
-              onClick={() => {
-                const content2 = document.getElementById("content2");
-                if (
-                  content2.style.display === "none" ||
-                  content2.style.display === ""
-                ) {
-                  content2.style.display = "block";
-                } else {
-                  content2.style.display = "none";
-                }
-              }}
-            >
-              See description
-            </button>
-            <div
-              class="content2"
-              style={{
-                display: "none",
-                padding: "0 18px",
-                overflow: "hidden",
-                backgroundColor: "#f1f1f1",
-              }}
-              id="content2"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <br />
+      
 
-            <div className="mx-auto d-block">
-              {" "}
-              <b>do you want to download the file?</b>
-              <button
-                href=""
-                download="downloaded-file-name.pdf"
-                className="btn btn-success"
-                style={{
-                  backgroundColor: "green",
-                  borderRadius: "8px",
-                  padding: "5px 15px",
-                }}
-              >
-                Download
-              </button>
-            </div>
-          </div>
-        </div>
+
+       
+
+       
       </div>
+
       <br></br>
       <br />
       <div
