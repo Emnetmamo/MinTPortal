8f117e65eeb809a7389df809d42c0881b51555da
one-two345 <129900924+one-two345@users.noreply.github.com>
2023-10-28 22:00:31 +0300

    news page is now dynamic

---
+++ server/models/news.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+let news_schema = new mongoose.Schema({
+  title:{type: String, required: true},
+  author: {type: String, required: true},
+  content: {type: String, required: true},
+  category: {type: String, required: true},
+  date: {type: Date, required: true},
+});
+
+const News = mongoose.model('News', news_schema);
+
+export default News
