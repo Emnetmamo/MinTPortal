aeaf4f7738361691628461751e2328b71db09c27
adan eshete <adaneeshete560@gmail.com>
2023-10-30 08:06:43 -0700

    database set

--- server/models/news.js
+++ server/models/news.js
@@ -1,13 +1,14 @@
-import mongoose from "mongoose";
+
+const mongoose=require("mongoose");
 
 let news_schema = new mongoose.Schema({
   title:{type: String, required: true},
   author: {type: String, required: true},
   content: {type: String, required: true},
   category: {type: String, required: true},
-  date: {type: Date, required: true},
+  date: {type: String, required: true},
 });
 
 const News = mongoose.model('News', news_schema);
 
-export default News
+module.exports= News
