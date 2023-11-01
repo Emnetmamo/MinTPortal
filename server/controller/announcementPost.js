fc2de42fabfbd2ebd6c80f071db995fefe27953b
Adam-Abera <iruleadamabera@gmail.com>
2023-11-01 16:04:08 +0300

    Updated announcement backend

--- server/controller/announcementPost.js
+++ server/controller/announcementPost.js
@@ -1,12 +1,27 @@
 import announcementModel from "../models/announcements";
 
 function announcementPost(req, res){
-    const {title,
-        description,
-        field,
-        startDate,
-        endDate,
-        prizes,
-        instructions,
-        guideline} = req.body;
+    if(req.params.page === "addCall")
+    {
+        const {title,
+            description,
+            field,
+            callType,
+            startDate,
+            endDate,
+            prizes,
+            instructions,
+            guideline} = req.body;
+        try{
+            announcementModel.create({title, description, field, callType, startDate, endDate, prizes, instructions, guideline});
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    else if(req.params.page === "fetchCalls"){
+        const 
+    }
 }
+
+export default announcementPost;
