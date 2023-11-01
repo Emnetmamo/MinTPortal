47333572a0733c459b41ee2549aaf2fa4fb954bd
Adam-Abera <iruleadamabera@gmail.com>
2023-11-01 13:19:05 +0300

    Fixed backend issues

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,7 +1,7 @@
-import express from 'express'
-const adminRoute=express.Router()
-import {addNews,getNews} from'../controller/adminControl.js'
-adminRoute.post('/admin/news/add-news',addNews)
-adminRoute.get('/news',getNews)
+// import express from 'express'
+// const adminRoute=express.Router()
+// import {addNews,getNews} from'../controller/adminControl.js'
+// adminRoute.post('/admin/news/add-news',addNews)
+// adminRoute.get('/news',getNews)
 
-export default adminRoute
+// export default adminRoute
