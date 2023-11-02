42b677698a18950f9c03ab68384309d067f8cd08
Yinges-Damtie <yingesdamtie@gmail.com>
2023-11-02 10:29:03 +0300

    update

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -4,16 +4,11 @@ import React from "react";
 import AboutUsSection from "../components/AboutUsComponents/AboutUsSection";
 import OrganizationStructure from "../components/AboutUsComponents/OrganizationStructure";
 import Leaders from "../components/AboutUsComponents/Leaders";
-import MapContainer from "../components/AboutUsComponents/MapContainer";
 
 const AboutUs = () => {
   return (
     <div>
       <br /> <br />
-      <div className="container shadow p-4 mb-4 bg-white rounded">
-        <h1>Use Google Map Here About Our Location</h1>{" "}
-      </div>
-      <MapContainer />
       <AboutUsSection />
       <OrganizationStructure />
       <Leaders />
