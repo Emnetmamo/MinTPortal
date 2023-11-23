7f16ae86cd0d7de889c943ec8ad4c3e4582955db
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2023-11-22 17:24:18 -0800

    Collaboration page UI changes

--- client/src/pages/Collaborations.js
+++ client/src/pages/Collaborations.js
@@ -9,37 +9,37 @@ import collab2 from '../images/Collaborations/kodica-logo.png';
 import collab3 from '../images/Collaborations/Logo-ACTS.jpg';
 import collab4 from '../images/Collaborations/jos-log.png';
 import collab5 from '../images/Collaborations/dereja-log.png';
-
+import "../App.css"
 
 const collaborationData = [
   {
-    id: 1,
+    id: "https://www.undp.org/",
     title: 'United Nations Development Program',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'UNDP works in about 170 countries and territories, helping to eradicate poverty, reduce inequalities and exclusion, and build resilience so countries can sustain progress. As the UN’s development agency, UNDP plays a critical role in helping countries achieve the Sustainable Development Goals.',
     imageUrl: collab1,
   },
   {
-    id: 2,
+    id: "https://www.koica.go.kr/sites/koica_en/index.do",
     title: 'Korean International Coorporation Agency',
-    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Contributing to the advancement of international cooperation through various cooperative projects to promote friendly and cooperative relationships and mutual exchanges between Republic of Korea and developing countries and achieve poverty reduction, improvement of the quality of life, sustainable development, and humanitarianism of developing countries.',
     imageUrl: collab2,
   },
   {
-    id: 3,
-    title: 'Afriacn Center of Technology Science ',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+    id: "http://www.acts-net.org/",
+    title: 'Afriacn Center of Technology Science',
+    description: 'ACTS vision is knowledge for better livelihoods. Its Mission is to strengthen the capacity and policies of African countries and institutions to harness science, technology and innovation for sustainable development.',
     imageUrl: collab3,
   },
   {
-    id: 4,
+    id: "https://mastercardfdn.org/fdre-jobs-creation-commission-launches-enabling-ethiopia-a-5-year-job-opportunities-project-in-partnership-with-the-mastercard-foundation/",
     title: 'Jobs Creation Commission Ethiopia',
-    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    description: 'Our work is guided by its mission to advance education and financial inclusion to catalyze prosperity in developing countries and to support Indigenous youth in Canada. It was created in 2006 by Mastercard International and operates independently under the governance of its own Board of Directors.',
     imageUrl: collab4,
   },
   {
-    id: 5,
+    id: "https://www.dereja.com/",
     title: 'Dereja',
-    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.',
+    description: 'Dereja.com is a social enterprise of Info Mind Solutions PLC. (Ethiojobs.net). Established in 2017, we are dedicated to supporting the over 200,000 fresh graduates entering the labor force in starting their careers on the right path.',
     imageUrl: collab5,
   }
 ];
@@ -71,18 +71,18 @@ const Collaborations = () => {
       <Slider {...settings}>
         {collaborationData.map(collaboration => (
           <div key={collaboration.id} className="px-2">
-            <div className="card rounded">
+            <div className="card rounded" style={{  height: "650px"}}>
               <img
                 src={collaboration.imageUrl}
                 className="card-img-top  text-center" // Added rounded-circle class
                 alt={`Collaboration ${collaboration.id}`}
-                style={{ width: '200px', height: '200px' }}
+                style={{ width: '200px', height: '200px' , marginLeft: "83px", marginTop:"10px"}}
               />
               <div className="card-body">
-                <h5 className="card-title">
-                  <a href={`/collaboration/${collaboration.id}`}>{collaboration.title}</a>
+                <h5 className="card-title" >
+                  <a style={{color:"orange"}} href={collaboration.id} target='_blank'>{collaboration.title}</a>
                 </h5>
-                <p className="card-text">{collaboration.description}</p>
+                <p style={{color:"black", fontWeight: "normal"}} className="card-text">{collaboration.description}</p>
               </div>
             </div>
           </div>
