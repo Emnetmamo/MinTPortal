2a0554d1da22cc34a55d224bbae4d01d36cedc03
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2023-11-23 20:32:29 -0800

    Feedback is sorted
    The comments are displayed by most recent at the top order :)

--- client/src/pages/admin/ViewFeedback.js
+++ client/src/pages/admin/ViewFeedback.js
@@ -149,16 +149,19 @@ const ViewFeedback = () => {
           </div>
           <div className="col-xs-12 col-md-2"></div>
           <div className="col-xs-12 col-md-7 mb-5">
-          {feedbackData.map((feedback) => (
-      <Card key={feedback.createdAt} className="mb-3" style={{background: "#F4EFEF"}}>
-        <Card.Body>
-          <Card.Subtitle className="mb-2 text-muted">{new Date(feedback.createdAt).toLocaleString()}</Card.Subtitle>
-           <Card.Title style={{fontSize:"24px"}}>{feedback.fullName}</Card.Title>
-            <Card.Subtitle className="mb-2" style={{color:"blue"}}>{feedback.email}</Card.Subtitle>
-            <Card.Text style={{color:"black", fontWeight:"normal"}} className="mb-2">{feedback.message}</Card.Text>
-        </Card.Body>
-      </Card>
-    ))}
+          {feedbackData
+              .slice() // Create a shallow copy of the array
+              .reverse() // Reverse the array
+              .map((feedback) => (
+                <Card key={feedback.createdAt} className="mb-3" style={{ background: "#F4EFEF" }}>
+                  <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">{new Date(feedback.createdAt).toLocaleString()}</Card.Subtitle>
+                    <Card.Title style={{ fontSize: "24px" }}>{feedback.fullName}</Card.Title>
+                    <Card.Subtitle className="mb-2" style={{ color: "blue" }}>{feedback.email}</Card.Subtitle>
+                    <Card.Text style={{ color: "black", fontWeight: "normal" }} className="mb-2">{feedback.message}</Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
           </div>
         </div>
       </div>
