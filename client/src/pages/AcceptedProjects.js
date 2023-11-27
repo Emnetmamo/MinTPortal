c43c800b1813987f94fc44d4a8dba1764475f729
Adam-Abera <iruleadamabera@gmail.com>
2023-11-27 21:59:02 +0300

    Searching works on all pages

--- client/src/pages/AcceptedProjects.js
+++ client/src/pages/AcceptedProjects.js
@@ -52,7 +52,18 @@ function getFileNameFromPath(filePath) {
         // Handle the error, maybe show a message to the user
       });
   };
-
+  function searchItem(e){
+    let searchText = e.value.toLowerCase();
+    let titles = document.getElementsByClassName('card-title');
+    Array.from(titles).forEach(function(title1){
+      if(title1.innerHTML.toLowerCase().indexOf(searchText) > -1){
+        title1.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "";
+      }
+      else{
+        title1.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+      }
+    })
+  }
   return (
     <div className="container m-10" >
       <br/><br/>
@@ -65,6 +76,7 @@ function getFileNameFromPath(filePath) {
           aria-label="Enter project title"
           aria-describedby="basic-addon2"
           style={{ maxWidth: '200px' }}
+          onChange={function(e){searchItem(e.target)}}
         />
         <div className="input-group-append mt-2">
           <span className="input-group-text bg-white border-0"><FaSearch /></span>
