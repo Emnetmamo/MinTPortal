820a67a32e4d179531fa0cb6cefbcadac93188a4
Yinges-Damtie <yingesdamtie@gmail.com>
2023-12-14 12:32:20 +0300

    Update ContactUs.js

--- client/src/components/AboutUsComponents/ContactUs.js
+++ client/src/components/AboutUsComponents/ContactUs.js
@@ -95,7 +95,7 @@ const ContactUs = () => {
           title="MinT Location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.8459669515486!2d38.7525032!3d9.0238125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0:0xd6d189c2f507323b!2s2QF4%2BG2G%2C%20%E1%8A%A0%E1%8B%B2%E1%88%B5%20%E1%8A%A0%E1%89%A0%E1%89%A3!5e0!3m2!1sen!2set!4v1637351575868!5m2!1sen!2set"
           width="100%"
-          height="300"
+          height="100%"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
