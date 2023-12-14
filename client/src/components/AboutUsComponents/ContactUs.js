9cf320d87ccdf64dd6cfefa69eb0c36825f3549a
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2023-12-13 16:39:50 -0800

    Update ContactUs.js

--- client/src/components/AboutUsComponents/ContactUs.js
+++ client/src/components/AboutUsComponents/ContactUs.js
@@ -1,25 +1,91 @@
 // ContactUs.jsx
-import React from 'react';
+import {React, useState} from 'react';
 import "../../images/assets/css/contactUs.css"
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+axios.defaults.withCredentials=true;
+
+
+
+
+
 
 
 const ContactUs = () => {
+
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    message: '',
+
+  });
+
+  //handle change event
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  //handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    
+      const data = new FormData();
+
+      
+      data.append('fullName', formData.fullName);
+      data.append('message', formData.message);  
+      data.append('email', formData.email);
+     
+
+      try {
+        const response = axios.post('http://localhost:5001/footer/add-footer', data);
+        console.log(response.data);
+          alert('Do you want to submit')
+          toast.info('footer form submitted successfully!');
+          // await  window.location.reload()
+      } catch (errors) {
+        console.error('Error:', errors.message);
+        toast.error('An error occurred while submitting footer form.');
+      }
+    
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-form">
         <h2>Contact Us</h2>
-        <form>
+        <form method='POST'  onSubmit={handleSubmit} encType='multipart/form-data' >
           <div className="form-group">
-            <label htmlFor="name">Your Name:</label>
-            <input type="text" id="name" name="name" required />
+            <label htmlFor="name">Full Name:</label>
+            <input type="text" id="fullName" name="fullName" 
+            required 
+            placeholder='Full name'
+            value={formData.fullName}
+            onChange={handleChange}/>
           </div>
+
           <div className="form-group">
-            <label htmlFor="email">Your Email:</label>
-            <input type="email" id="email" name="email" required />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" 
+            required 
+            placeholder="Enter Your Email"
+            value={formData.email}
+            onChange={handleChange}/>
           </div>
+
           <div className="form-group">
             <label htmlFor="message">Your Message:</label>
-            <textarea id="message" name="message" rows="4" required></textarea>
+            <textarea id="message" name="message" rows="4" 
+            required
+            placeholder="Message"
+            value={formData.message}
+            onChange={handleChange}></textarea>
           </div>
           <button type="submit">Send Message</button>
         </form>
