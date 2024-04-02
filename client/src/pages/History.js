17ed04f94adee176ce67b28dd56cfe4993a2d789
adan eshete <adaneeshete560@gmail.com>
2024-04-02 21:06:23 +0300

    history

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -3,7 +3,12 @@ import React from 'react'
 const History = () => {
   return (
     <div>
-      
+      <br/>
+      <br/>
+      <br/>
+      <div>
+        history is going to done my mint
+      </div>
     </div>
   )
 }
