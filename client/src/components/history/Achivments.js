08b9efdb0be376c8cf31b0ab1e07c87926e23b73
ebisadugna <130274241+ebisadugna@users.noreply.github.com>
2024-04-03 15:25:37 +0300

    view pdf update

--- client/src/components/history/Achivments.js
+++ client/src/components/history/Achivments.js
@@ -5,30 +5,26 @@ import axios from 'axios';
 const Achivments = () => {
   const { id } = useParams(); 
   const [achievement, setAchievement] = useState(null); 
+  const [error, setError] = useState(null); // State for handling errors
 
   useEffect(() => {
-    
     axios.post('http://localhost:5001/history/homehistory', { id })
       .then(response => {
         setAchievement(response.data.data);
-        console.log(response.data.data)
       })
       .catch(error => {
-        
+        setError(error); // Set error state if request fails
         console.error('Error fetching data:', error);
       });
   }, [id]);
 
   return (
     <div className="container mt-5">
+      {error && <div className="alert alert-danger">Error fetching data: {error.message}</div>}
       {achievement && (
         <div className="card">
           <div className="card-body">
-            
-          <img src={achievement.imagePath} className="img-fluid" alt="Achievement"
-           style={{width:'70%', height:'auto',
-           markerStart:'30px'
-           }} />
+            <img src={achievement.imagePath} className="img-fluid" alt="Achievement" style={{width:'70%', height:'auto'}} />
             <h5 className="card-title">{achievement.title}</h5>
             <p className="card-text">Investigator: {achievement.p_investigator}</p>
             <p className="card-text">Author: {achievement.author}</p>
