bbbea9b12f96416ee4957a13e0c4ff0c7250c4b1
adan eshete <adaneeshete560@gmail.com>
2024-04-03 14:51:43 +0300

    commint

--- client/src/components/history/Achivments.js
+++ client/src/components/history/Achivments.js
@@ -3,18 +3,18 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Achivments = () => {
-  const { id } = useParams(); // Destructure id from useParams()
-  const [achievement, setAchievement] = useState(null); // State to store the fetched achievement
+  const { id } = useParams(); 
+  const [achievement, setAchievement] = useState(null); 
 
   useEffect(() => {
-    // Make axios request when id changes
+    
     axios.post('http://localhost:5001/history/homehistory', { id })
       .then(response => {
-        // Set the fetched achievement in state
         setAchievement(response.data.data);
+        console.log(response.data.data)
       })
       .catch(error => {
-        // Handle error
+        
         console.error('Error fetching data:', error);
       });
   }, [id]);
@@ -24,6 +24,11 @@ const Achivments = () => {
       {achievement && (
         <div className="card">
           <div className="card-body">
+            
+          <img src={achievement.imagePath} className="img-fluid" alt="Achievement"
+           style={{width:'70%', height:'auto',
+           markerStart:'30px'
+           }} />
             <h5 className="card-title">{achievement.title}</h5>
             <p className="card-text">Investigator: {achievement.p_investigator}</p>
             <p className="card-text">Author: {achievement.author}</p>
@@ -31,7 +36,7 @@ const Achivments = () => {
             <p className="card-text">Description: {achievement.description}</p>
             <p className="card-text">Field of Study: {achievement.field_of_study}</p>
             <p className="card-text">Deffence Date: {new Date(achievement.date).toLocaleDateString()}</p>
-            <img src={achievement.imagePath} className="img-fluid" alt="Achievement" />
+    
             <a href={achievement.filePath} className="btn btn-primary mt-3" target="_blank" rel="noopener noreferrer">View PDF</a>
           </div>
         </div>
